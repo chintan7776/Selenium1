5d6784c341606911f0df6b771af09d4ec83923b5
Chintan Patel <Chintanpatel7776@gmail.com>
2022-09-14 20:08:44 +0100

    Selenium1

--- A_WebDriverBasic.java
+++ A_WebDriverBasic.java
@@ -1,4 +1,5 @@
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -9,6 +10,14 @@ public class A_WebDriverBasic {
         System.setProperty("webdriver.edge.driver","C:\\software\\driver\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
         driver.get("https://demo.nopcommerce.com/");
+
+        driver.findElement(By.className("ico-login")).click();
+        driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("chi@gmail.com");
+
+        driver.findElement(By.id("Password")).sendKeys("123");
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+
+
 
 
     }
--- C_WebDriverGo.java
+++ C_WebDriverGo.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,10 @@ public class C_WebDriverGo {
         System.setProperty("webdriver.chrome.driver","C:\\software\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.nopcommerce.com/");
+        driver.findElement(By.className("ico-login")).click();
+        driver.findElement(By.id("Email")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("12345");
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
 
 
     }
