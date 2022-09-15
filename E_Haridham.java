5948505852365a435579a8c9f91fcb207c2402b9
Chintan Patel <Chintanpatel7776@gmail.com>
2022-09-15 19:46:24 +0100

    Selenium1

---
+++ E_Haridham.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class E_Haridham {
+    public static void main(String[] args ) {
+        System.setProperty("webdriver.chrome.driver","C:\\software\\driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ydsuk.org//");
+        driver.findElement(By.xpath("//*[@id=\"page-container\"]/div[1]/div/div/div/div[1]/div/div/div/a")).click();
+        System.out.println(driver.getTitle());
+        String Title = driver.getTitle();
+        System.out.println("Page Title:" +Title);
+        driver.navigate().back();
+
+
+
+    }
+
+}
