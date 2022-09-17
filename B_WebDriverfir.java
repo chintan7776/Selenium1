870254a796dcfd9159d91a9bd9c2de3dfc32b385
Chintan Patel <Chintanpatel7776@gmail.com>
2022-09-17 20:24:32 +0100

    Selenium1

--- B_WebDriverfir.java
+++ B_WebDriverfir.java
@@ -7,6 +7,7 @@ public class B_WebDriverfir {
         public static void main(String[] args) {
             System.setProperty("webdriver.gecko.driver", "C:\\software\\driver\\geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
-            driver.get("https://demo.nopcommerce.com/");
+            driver.get("https://www.amazon.co.uk/");
+
         }
 }
--- D_RegPage.java
+++ D_RegPage.java
@@ -1,3 +1,7 @@
+
+
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,7 +14,7 @@ public class D_RegPage {
         System.setProperty("webdriver.chrome.driver", "C:\\software\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.nopcommerce.com/");
-        driver.findElement(By.className("ico-register")).click();
+        driver.findElement(By.xpath("//a[@class='ico-register']")).click();
         driver.findElement(By.id("gender-male")).click();
         driver.findElement(By.id("FirstName")).sendKeys("Chintan");
         driver.findElement(By.id("LastName")).sendKeys("Patel");
